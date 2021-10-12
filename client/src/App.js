c163e4975c49b205e9be36949bf65098497145ab
rcraig <ryanpcraig1@gmail.com>
2021-10-11 22:06:30 -0500

    Nav bar improvments

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-
+import Nav from './components/Nav';
+// import { Switch, Route} from 'react-router-dom';
+// import Home from './pages/Home'
+// import Cart from './pages/Cart'
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
+      {/* <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/cart" component={Cart} />
+      </Switch> */}
     </div>
   );
 }
---
+++ client/src/components/Nav.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { AppBar, Toolbar, CssBaseline, Typography, makeStyles } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+  logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(7.5),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" style={{backgroundColor: "#3b3b3d"}}>
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+          Gregslist
+        </Typography>
+          <div className={classes.navlinks}>
+            <Link to="/" className={classes.link}>
+              Home
+            </Link>
+            <Link to="/cart" className={classes.link}>
+              Cart
+            </Link>
+          </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default Navbar;
