6a72a93669cd602d1461275caed1144b56bcfeb2
rcraig <ryanpcraig1@gmail.com>
2021-10-12 19:42:10 -0500

    rough draft for card completed

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchProducts } from '../store/actions/ProductActions'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Button from '@mui/material/Button';
 
 const mapStateToProps = ( productState ) => ({
   ...productState
@@ -22,7 +32,36 @@ const Products = (props) => {
     <div>
       {props.productState.products.products ? (
         props.productState.products.products.map((product) => (
-          <h1 key={product._id}>Product Name: {product.title}</h1>
+          <Card sx={{ maxWidth: 350 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  R
+                </Avatar>
+              }
+              title={product.title}
+              subheader={"Price $" + product.price}
+            />
+            {/* <CardMedia
+              component="img"
+              height="194"
+              image="/static/images/cards/paella.jpg"
+              alt="Paella dish"
+            /> */}
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {"Description: " + product.description}
+              </Typography>
+              
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <Button size="small">Add to Cart</Button>
+            </CardActions>
+          </Card>
+          // <h1 key={product._id}>Product Name: {product.title}</h1>
         ))
       ) : (
         null
