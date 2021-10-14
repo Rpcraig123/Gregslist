24e35fc27314e66acd02fcdf533d6e82b552d1bc
rcraig <ryanpcraig1@gmail.com>
2021-10-14 12:12:09 -0500

    back end routes added

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,14 @@ router.get('/cart/:userId', controllers.getCart)
 
 router.post('/new-product-cart/:productId', controllers.addToCart)
 
+// router.delete('/rem-product-cart/:productId', controllers.remCart)
+
 router.post('/new-user', controllers.addUser)
 
 router.get('/users', controllers.getUsers)
 
 // router.post('/new-comment', controllers.addComment)
+
+// router.delete('/rem-comment', controllers.remComment)
 
 module.exports = router
