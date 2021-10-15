a1234289ecfa981a43f084e84786a21facfc8a0c
rcraig <ryanpcraig1@gmail.com>
2021-10-14 22:06:01 -0500

    started sign in page

---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { SignInUser } from '../../services/Auth'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+export default function SignIn(props) {
+
+  const [formValues, setFormValues] = useState({ username: '', password: '' })
+  
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(formValues)
+    setFormValues({ username: '',password: '' })
+    props.setUser(payload)
+    props.toggleAuthenticated(true)
+    props.history.push('/')
+  }
+
+  return (
+    <div className="signin-sect">
+      <h1>SIGN IN</h1>
+        <Form className="signin-form" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label>Username</Form.Label>
+            <Form.Control onChange={handleChange} name="username" type="text" value={formValues.username} required/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onChange={handleChange} type="password" name="password" value={formValues.password} required/>
+          </Form.Group>
+          <Button type="submit" disabled={!formValues.username || !formValues.password}>Sign In</Button>
+        </Form>
+    </div>
+  )
+}
